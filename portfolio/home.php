4556c9a0c6b2374c4d13c8c0729fe2d543a04e7d
Ana Beatriz Buzana <132920774+Buzana@users.noreply.github.com>
2024-03-06 08:58:28 -0300

    Add files via upload

---
+++ portfolio/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfólio - Ana</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class ="div1">
+        <div class="divInfoPess">
+            <div class="imge"></div>
+            <div class="div2">
+                <div class="info1"><h1>Nome:</h1></div>
+                <div class="info1"><h1>Data de nascimento:</h1></div>
+                <div class="info1"><h1>Endereço:</h1></div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
